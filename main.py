1a7053113ffd795327f7e740be1edac4660555b7
Canakin <46483633+Canakin@users.noreply.github.com>
2019-01-08 15:16:14 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Can Cimenser's Favorite Song Project
+The project presents all of the features of my favorite song 'One'
+"""
+
+Song = "One"
+Genre = "Trash Metal"
+Artist = "Metallica"
+Album = "And Justice For All"
+DurationInMinutes = 7.45
+Year_Released = 1988
+No_of_instruments = 3
+Spotifty_play_mn = 135.5
+# I created all the variables above 
+
+#I print all the features of the song below:
+print("My favorite song is ",Song)
+print("The genre of the song is ",Genre)
+print("The artist of the song is ",Artist)
+print("The album of the song is ",Album) 
+print("The duration of the song is ", DurationInMinutes," minutes")
+print("The song was released in ",Year_Released)
+print("The number of instruments used in the song is ",No_of_instruments)
+print("The sons has been played ",Spotifty_play_mn," times on Spotify")
+
