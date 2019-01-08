afe54ae1f699a9ff444e5a0447f527770c101296
Canakin <46483633+Canakin@users.noreply.github.com>
2019-01-08 19:58:29 +0300

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,25 @@
+"""
+The functions
+Features of the favorite song
+"""
+
+genre = "Trash Metal"
+artist = "Metallica"
+year = 1988
+# Here I defined global variables above
+
+def song_genre():
+    print(genre)
+def song_artist():
+    print(artist)
+def song_year():
+    print(year)
+# Here I defined the functions above
+
+song_genre()
+song_artist()
+song_year()
+
+
+  
+    
